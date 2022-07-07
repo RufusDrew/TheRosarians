85e533fd27ea801dea845820aeed6978bb455004
rufus drew <rufusdrew3@gmail.com>
2022-07-07 10:09:54 +0530

    upload songs

--- src/mocks/songs.js
+++ src/mocks/songs.js
@@ -13,6 +13,27 @@ const songs = [
     cover: require("../assets/images/pst.jpg"),
     src: require("../assets/audios/pst.mp3")
   },
+  {
+    title: "THE GLORIOUS MYSTERIES OF THE ROSARY",
+    artist: "CatholicLink",
+    seconds: 1440,
+    cover: require("../assets/images/Glorious.png"),
+    src: require("../assets/audios/Rosary-Glorious-Mysteries.mp3")
+  },
+  {
+    title: "THE LUMINOUS MYSTERIES OF THE ROSARY",
+    artist: "CatholicLink",
+    seconds: 1380,
+    cover: require("../assets/images/thebaptismjordan.png"),
+    src: require("../assets/audios/Rosary-Luminous-Mysteries.mp3")
+  },
+  {
+    title: "THE SORROWFUL MYSTERIES OF THE ROSARY",
+    artist: "CatholicLink",
+    seconds: 1320,
+    cover: require("../assets/images/Sorrowful-1.png"),
+    src: require("../assets/audios/Rosary-Sorrowful-Mysteries.mp3")
+  },
 ];
 
 export default songs;
