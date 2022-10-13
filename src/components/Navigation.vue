6e9d75ffca899e1b85774b43943bf25063a3abba
rufus drew <rufusdrew3@gmail.com>
2022-10-13 14:38:08 +0530

    pos-20.3

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -142,6 +142,8 @@ header {
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
   z-index: 99;
+  position: fixed;
+  width: 100%;
   .link {
     font-weight: 500;
     padding: 0 8px;
