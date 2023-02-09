2e173eca7e4d273799cd037c77418b50f6a9906c
rufus drew <rufusdrew3@gmail.com>
2023-02-09 22:07:32 +0530

    sw2

--- src/main.js
+++ src/main.js
@@ -30,7 +30,6 @@ if ('serviceWorker' in navigator) {
   // register the service worker
   navigator.serviceWorker.register('service-worker.js')
       .then(function(reg){
-          console.log("service worker registered");
       }).catch(function(err) {
           console.log("error: ", err)
       });
