28ac2e443a04765002275c138fbe5d89b8324237
rufus drew <rufusdrew3@gmail.com>
2023-02-10 01:11:59 +0530

    sw-new-upd

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const manifestJSON = require("./public/manifest.json");
 module.exports = {
   chainWebpack: (config) => {
     const svgRule = config.module.rule("svg");
@@ -11,13 +12,31 @@ module.exports = {
       .use("vue-svg-loader")
       .loader("vue-svg-loader");
   },
-  pwa: {
-    themeColor: "#42b983",
-    msTileColor: "#42b983",
-    appleMobileWebAppCache: "yes",
-    manifestOptions: {
-      background_color: "#42b983"
-    }
-  }
+};
 
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: true,
+    },
+  },
+
+  runtimeCompiler: true,
+
+  pwa: {
+    background_color: manifestJSON.background_color,
+    name: manifestJSON.short_name,
+    msTileColor: manifestJSON.background_color,
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "service-worker.js",
+      exclude: [/_redirects$/],
+    },
+  },
 };
