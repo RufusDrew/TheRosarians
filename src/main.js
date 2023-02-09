ba30e53903c661325d99dbdaff7f112061617813
rufus drew <rufusdrew3@gmail.com>
2023-02-09 22:07:06 +0530

    sw

--- src/main.js
+++ src/main.js
@@ -26,6 +26,16 @@ Vue.use(Vue2Editor);
 
 Vue.config.productionTip = false;
 
+if ('serviceWorker' in navigator) {
+  // register the service worker
+  navigator.serviceWorker.register('service-worker.js')
+      .then(function(reg){
+          console.log("service worker registered");
+      }).catch(function(err) {
+          console.log("error: ", err)
+      });
+}
+
 let app;
 firebase.auth().onAuthStateChanged(() => {
   if (!app) {
