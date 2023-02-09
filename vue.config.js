ccd9ff8b19218d59e91618fa70da1cdca4884c70
rufus drew <rufusdrew3@gmail.com>
2023-02-10 01:59:05 +0530

    sw-ch

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,16 @@ module.exports = {
     msTileColor: "#42b983",
     appleMobileWebAppCache: "yes",
     manifestOptions: {
-      background_color: "#42b983"
-    }
-  }
+      background_color: "#42b983",
+    },
+  },
+};
+
+module.exports = {
+  pwa: {
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "./src/service-worker.js",
+    },
+  },
 };
